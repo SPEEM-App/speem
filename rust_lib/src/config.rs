ee933165a88fe513a4b3a8d8f41b9b9f09ba7aa3
Wassim Mansouri <wassim.mansouri@gmail.com>
2024-06-28 22:34:29 +0200

    Add basic schema migration initialization Use SQLCipher extension Update Configuration

--- rust_lib/src/config.rs
+++ rust_lib/src/config.rs
@@ -1,17 +1,19 @@
 use once_cell::sync::Lazy;
 use std::sync::Mutex;
+use serde::{Deserialize, Serialize};
+use std::fs;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Config {
     pub database_url: String,
-    // Add other configuration fields as needed
+    pub database_passphrase: String,
 }
 
 impl Default for Config {
     fn default() -> Self {
         Config {
-            database_url: "sqlite::memory:".to_string(),
-            // Initialize other fields with default values
+            database_url: "speem.db".to_string(),
+            database_passphrase: "default_passphrase".to_string(),
         }
     }
 }
@@ -25,4 +27,11 @@ pub fn get_config() -> Config {
 pub fn set_config(new_config: Config) {
     let mut config = CONFIG.lock().unwrap();
     *config = new_config;
+}
+
+pub fn load_config_from_file(file_path: &str) -> std::io::Result<()> {
+    let config_content = fs::read_to_string(file_path)?;
+    let new_config: Config = serde_json::from_str(&config_content)?;
+    set_config(new_config);
+    Ok(())
 }
--- rust_lib/src/errors.rs
+++ rust_lib/src/errors.rs
@@ -2,24 +2,24 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum AppError {
-    #[error("Database error")]
-    DatabaseError(#[from] sqlx::Error),
+    #[error("Database error: {0}")]
+    DatabaseError(#[from] rusqlite::Error),
 
     #[error("Crypto error")]
     CryptoError,
 
-    #[error("Configuration error")]
-    ConfigError(String),
+    #[error("Configuration error: {0}")]
+    ConfigError(#[from] std::io::Error),
 
-    #[error("Unknown error")]
-    Unknown,
+    #[error("Serialization error: {0}")]
+    SerializationError(#[from] serde_json::Error),
+
+    #[error("Unexpected error")]
+    UnexpectedError,
 }
-
 
 impl From<ring::error::Unspecified> for AppError {
     fn from(_: ring::error::Unspecified) -> Self { AppError::CryptoError }
 }
-
-
 
 pub type Result<T> = std::result::Result<T, AppError>;
